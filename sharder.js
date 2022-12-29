d24b2685966725fea27ae3603a7fff7d1263e040
Pheonix <97115569+Pheonix14@users.noreply.github.com>
2022-12-29 17:41:03 +0000

    Commit

--- sharder.js
+++
@@ -1,7 +0,0 @@
-const { ShardingManager } = require('discord.js');
-const { token } = require('./config/config.json')
-const manager = new ShardingManager('./index.js', { token: `${token}` });
-
-manager.on('shardCreate', shard => console.log(`Launched Shard ${shard.id}`));
-
-manager.spawn();
